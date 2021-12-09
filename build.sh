2affec869d87de663e571aa728d231ad61e32ce6
David Mathews <dgma224@g.uky.edu>
2021-12-09 13:00:55 -0500

    modified the build.sh script to build the .so file for linux

--- build.sh
+++ build.sh
@@ -1,4 +1,1 @@
-h5cc -std=c99 -fPIC -shared -o nabCompression.so nabCompression.c
-h5cc -std=c99 -fPIC -shared -o hdf5_dl.so hdf5_dl.c
-h5cc -fPIC -o test testCode.c nabCompression.o
-#h5cc -std=c99 -fPIC -shared -o nabCompression.so nabCompression.o
+h5cc -std=c99 -fPIC -shared -o src/nabCompression.so src/nabCompression.c
