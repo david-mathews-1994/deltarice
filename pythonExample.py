eb0e43bca6b11a377caaba817213adbbb4da41db
David Mathews <dgma224@g.uky.edu>
2021-12-10 16:27:52 -0500

    modified the behavior of the algorithm so that in the case where the user doesn't pass waveform length information or rice tuning parameter information it defaults to a mode safe for h5py integration

--- pythonExample.py
+++ pythonExample.py
@@ -6,29 +6,23 @@ RiceParameter=8
 WaveformLength=5
 compression_opts = (RiceParameter, WaveformLength)
 dtype=np.int16
-fullsize = (5, 5)
-chunksize = (5, 5)
+fullsize = (100, 100, 100)
+chunksize = (10, 10, 10)
 
 f = h5py.File('testFile.h5', 'w')
+'''
 dataset = f.create_dataset('data', 
 	fullsize, 
 	compression=nabCompression.h5.H5FILTER,
 	compression_opts=compression_opts,
 	dtype=dtype,
 	chunks=chunksize)
-
+'''
+dataset = f.create_dataset('data', fullsize, compression=nabCompression.h5.H5FILTER, dtype=dtype)
 array = np.random.normal(0, 10, size=fullsize).astype(dtype)
-
 dataset[:] = array
-
 f.close()
-
 f = h5py.File('testFile.h5', 'r')
 dataset = f['data']
-
-raw_bytes = dataset.id.read_direct_chunk((0, 0))[1]
-
-buffer = np.frombuffer(raw_bytes, dtype=np.int8)
-waveformBit = buffer.view(np.int16)[12:].view(np.uint32)
 
 print(np.array_equal(dataset[()], array))
